408d187edb21f413f3c7beb30b33cdeaded4b2fd
yeefy <yftran@gmail.com>
2014-01-28 21:57:02 -0800

    less jQuery

--- application.js
+++ application.js
@@ -1,14 +1,36 @@
 // JavaScript Document
+
+var itemsList = [ ];
+
 $(document).ready(function(e) {
+	$('#input').focus();
+	
 	$("#input").keypress(function(e) {
         if(e.keyCode==13){
-			$("#submit").click();
+			submit()
 		};
     });
 	
+	$('#submit').click(submit);
+
+function submit(){
+	var input = (document.getElementById('input').value);
+	addItem(input);
+	$('#input').val("");
+}
+
+
+function addItem(input){
+	itemList.push(input);
+	var lastItem = ((itemList.length)-1);
+	itemList[lastItem];
+	var newItemOnList = "<li>" + itemList[lastItem]+ "</li>";
+	$('#items').append('newItemOnList');
+}
+
 	
 	//add an item to the list
-	$('#submit').click(function(e) {
+	//$('#submit').click(function(e) {
 			
 				//var itemList = new Array[input];
 			
@@ -19,17 +41,17 @@ $(document).ready(function(e) {
 				//console.log(input);
 				//$('#items').append(input);
 					
-					var input = $('#input').val();
-					$('#items').append('<li>'+input+'<input type=button class="remove_item" value = "X"/></li>');
-					$("#input").val("");
-					$("#input").focus();
-			});
+					//var input = $('#input').val();
+					//$('#items').append('<li>'+input+'<input type=button class="remove_item" value = "X"/></li>');
+					//$("#input").val("");
+					//$("#input").focus();
+			//});
 			
 			
 	//remove an item from the list
-	$('#items').on('click', '.remove_item', function(){
-		$(this).parent().remove();
-	});
+	//$('#items').on('click', '.remove_item', function(){
+		//$(this).parent().remove();
+	//});
 });
 	
 
