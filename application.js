3b7176f273f09c1ac07a54f0f8fcc2d13cd6c53f
yeefy <yftran@gmail.com>
2014-02-02 22:33:31 -0800

    prevent empty input

--- application.js
+++ application.js
@@ -16,47 +16,38 @@ $(document).ready(function(e) {
 
 	
 //adding items to list
-	
-	function addItem(input){
-		itemsList.push(input);
-		var lastItem = ((itemsList.length)-1);
-		itemsList[lastItem];
-		$('#items').append('<li>' + "<input id='checkbox' type='checkbox'>" + itemsList[lastItem]+ '</li>');
-	}
-	
-	function submit(){
+	function addItem(){
 		var input = document.getElementById('input').value;
+//verify that input is not empty
+		if(input==""){
+			return(false);
+		}else{
+			itemsList.push(input);
+			var lastItem = ((itemsList.length)-1);
+			itemsList[lastItem];
+			$('#items').append('<li>' + "<input id='checkbox' type='checkbox'>" + itemsList[lastItem]+ '</li>')			
+		};
+	};
+			
+	function submit(){
 		addItem(input);
 		$('#input').val("");
-	}
+	};
 
 //remove item from list
 	
 	$(document).on('click', '#remove_button', function(){
 		$('#items input:checked').parent().remove();
 	});
-
-//verify that something is entered	
-	function verify(){
-		if(input==""){
-			alert("Enter something");
-			return(false);
-		}else{
-			addItem(input);
-		};
-			
-	}	
-
+	
 //verify that input is not repeated
 	function nonRepeat(){
 		var index = itemsList.indexOf(input);
 		var indexValue = itemsList[index];
 		for (i=0; i<itemsList.length;i++){
 			if(input==indexValue[i]){
-				alert("You already have that on the list!")
-			}else{
-				addItem(input);
-			};
+			alert("You already have that on the list!")
+		};
 		};
 	};
 });
