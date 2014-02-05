886c798476ea86218280d578b616aa713516799f
yeefy <yftran@gmail.com>
2014-02-04 23:16:48 -0800

    Final changes

--- application.js
+++ application.js
@@ -6,50 +6,53 @@ $(document).ready(function(e) {
 	
 	$('#input').focus();
 	
+	//use enter key to submit
 	$("#input").keypress(function(e) {
         if(e.keyCode==13){
-			submit()
+			$('#submit').click();
 		};
     });
-	
-	$('#submit').click(submit);
 
 	
 //adding items to list
-	function addItem(){
+	$('#submit').click(function(){
+		
 		var input = document.getElementById('input').value;
-//verify that input is not empty
-		if(input==""){
+		var index = itemsList.indexOf(input);
+		var indexValue = itemsList[index];
+		
+		//verify that item is not repeated
+		for (i=0; i<itemsList.length;i++){
+			if(input==indexValue){
+			alert("You already have that on the list!")
+			$('#input').val("");
 			return(false);
-		}else{
+			};
+		};
+		
+		//prevent empty inputs
+		if(input==""||input==" "||input=="   "){
+			return(false);
+		}
+		//add item to list
+		else{
 			itemsList.push(input);
 			var lastItem = ((itemsList.length)-1);
-			itemsList[lastItem];
-			$('#items').append('<li>' + "<input id='checkbox' type='checkbox'>" + itemsList[lastItem]+ '</li>')			
+			$('#items').append('<li>' + "<input id='checkbox' type='checkbox' index='"+lastItem+"'>" + itemsList[lastItem]+ '</li>')			
 		};
-	};
-			
-	function submit(){
-		addItem(input);
 		$('#input').val("");
-	};
+	});
+
+	
 
 //remove item from list
 	
 	$(document).on('click', '#remove_button', function(){
 		$('#items input:checked').parent().remove();
+		itemsList.remove($('#items input:checked').attr('index'));          
 	});
 	
-//verify that input is not repeated
-	function nonRepeat(){
-		var index = itemsList.indexOf(input);
-		var indexValue = itemsList[index];
-		for (i=0; i<itemsList.length;i++){
-			if(input==indexValue[i]){
-			alert("You already have that on the list!")
-		};
-		};
-	};
+	
 });
 	
 
